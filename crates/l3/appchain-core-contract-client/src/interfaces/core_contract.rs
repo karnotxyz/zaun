c2a2a565d8365733ef88f8dab2be84c7c0248d93
Arun Jangra <arunjangra1001@gmail.com>
2025-01-14 18:01:55 +0530

    feat : updated onchain_data_size param

--- crates/l3/appchain-core-contract-client/src/interfaces/core_contract.rs
+++ crates/l3/appchain-core-contract-client/src/interfaces/core_contract.rs
@@ -1,6 +1,7 @@
 use appchain_utils::invoke_contract;
 use appchain_utils::LocalWalletSignerMiddleware;
 use color_eyre::Result;
+use starknet_core::types::U256;
 use starknet_core::types::{Felt, InvokeTransactionResult};
 
 pub struct CoreContract {
@@ -18,13 +19,14 @@ impl CoreContract {
         snos_output: Vec<Felt>,
         program_output: Vec<Felt>,
         onchain_data_hash: Felt,
-        onchain_data_size: Felt,
+        onchain_data_size: U256,
     ) -> Result<InvokeTransactionResult> {
         let mut calldata = Vec::with_capacity(snos_output.len() + program_output.len() + 2);
         calldata.extend(snos_output);
         calldata.extend(program_output);
         calldata.push(onchain_data_hash);
-        calldata.push(onchain_data_size);
+        calldata.push(onchain_data_size.low().into());
+        calldata.push(onchain_data_size.high().into());
 
         invoke_contract(&self.signer, self.address, "update_state", calldata).await
     }
